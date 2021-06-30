1f685244d67d2df0de12b3fea802df7af722c0a4
Tony Yao <tony24862486@gmail.com>
2021-07-01 01:26:49 +0800

    fix lint error

--- app/services/mission.py
+++ app/services/mission.py
@@ -1,5 +1,5 @@
 import logging
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Optional
 from app.core.database import Machine, Mission
 from fastapi.exceptions import HTTPException
 from app.models.schema import MissionCreate, MissionUpdate
@@ -12,29 +12,28 @@ async def get_missions() -> List[Mission]:
     return missions
 
 
-async def get_mission_by_id(id: int) -> Mission:
-    item = await Mission.objects.get(id=id)
+async def get_mission_by_id(id: int) -> Optional[Mission]:
+    item = await Mission.objects.get_or_none(id=id)
     return item
 
 
 async def update_mission_by_id(id: int, dto: MissionUpdate):
-    try:
-        mission = await get_mission_by_id(id)
-    except:
+    mission = await get_mission_by_id(id)
+    if mission is None:
         raise HTTPException(
-            status_code=400, detail="cannot get mission by the id",
+            status_code=400, detail="cannot get a mission by the id",
         )
 
     updateDict: Dict[str, Any] = {}
     try:
-        if dto.name != None:
+        if dto.name is not None:
             updateDict["name"] = dto.name
 
-        if dto.machine_id != None:
+        if dto.machine_id is not None:
             machine = await get_machine_by_id(dto.machine_id)
             updateDict["machine"] = machine
 
-        if dto.description != None:
+        if dto.description is not None:
             updateDict["description"] = dto.description
 
         await mission.update(None, **updateDict)
