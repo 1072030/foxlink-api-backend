93ee0db66f31086c4958e142eb1c8c9db3dc37c1
Tony Yao <tony24862486@gmail.com>
2022-02-13 17:20:21 +0800

    feat: mqtt publish add retain feature

--- app/mqtt/main.py
+++ app/mqtt/main.py
@@ -1,14 +1,15 @@
 from paho.mqtt import client as mqtt_client
 from app.env import MQTT_PORT, MQTT_BROKER
+import logging
 import json
 
 
 def connect_mqtt(broker: str, port: int, client_id: str):
     def on_connect(client, user_data, flags, rc):
         if rc == 0:
-            print("Connected to MQTT broker")
+            logging.log("Connected to MQTT broker")
         else:
-            print("Failed to connect to MQTT, returnee code: ", rc)
+            logging.error("Failed to connect to MQTT, returnee code: ", rc)
 
     c = mqtt_client.Client(client_id, protocol=mqtt_client.MQTTv5)
     c.on_connect = on_connect
@@ -20,11 +21,11 @@ def connect_mqtt(broker: str, port: int, client_id: str):
 client = connect_mqtt(MQTT_BROKER, MQTT_PORT, "foxlink_api_server")
 
 
-def publish(topic: str, payload, qos: int = 0) -> bool:
+def publish(topic: str, payload, qos: int = 0, retain: bool = False) -> bool:
     if client is None:
         raise Exception("MQTT client is not initialized")
 
     json_str = json.dumps(payload)
-    result = client.publish(topic, payload=json_str, qos=qos)
+    result = client.publish(topic, payload=json_str, qos=qos, retain=retain)
     # if result[0] is 0, then publish successfully
     return result[0] == 0
