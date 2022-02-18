93773202bff89be9689f7c623e9a0c422f594fdc
Tony Yao <tony24862486@gmail.com>
2022-02-18 20:55:10 +0800

    fix: start_date & end_date should work now

--- app/routes/log.py
+++ app/routes/log.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 import datetime
 from app.core.database import AuditActionEnum, AuditLogHeader
+from typing import Optional
 
 router = APIRouter(prefix="/logs")
 
@@ -8,7 +9,14 @@ router = APIRouter(prefix="/logs")
 @router.get("/", tags=["logs"])
 async def get_logs(
     action: AuditActionEnum,
-    start_date: datetime.datetime,
-    end_date: datetime.datetime,
+    start_date: Optional[datetime.datetime] = None,
+    end_date: Optional[datetime.datetime] = None,
 ):
-    return await AuditLogHeader.objects.filter(action=action).all()
+    params = {
+        "action": action.value,
+        "created_date__gte": start_date,
+        "created_date__lte": end_date,
+    }
+    params = {k: v for k, v in params.items() if v is not None}
+
+    return await AuditLogHeader.objects.filter(**params).all() # type: ignore
