4df0438cbc6ec188244dbe98e24fa4a6d692cc8f
Tony Yao <tony24862486@gmail.com>
2022-02-24 02:36:19 +0800

    feat: update statistics

--- app/routes/statistics.py
+++ app/routes/statistics.py
@@ -1,10 +1,12 @@
+import datetime
 from typing import List, Any
 from fastapi import APIRouter
 from pydantic import BaseModel
+from app.core.database import WorkerStatus, WorkerStatusEnum
 
 from app.services.statistics import (
     get_top_most_crashed_devices,
-    get_login_users_percentage_today,
+    get_login_users_percentage_by_week,
     get_top_most_reject_mission_employee,
 )
 
@@ -14,7 +16,7 @@ router = APIRouter(prefix="/stats")
 class Stats(BaseModel):
     top_most_crashed_devices: List[Any]
     top_most_reject_mission_employee: List[Any]
-    login_users_percentage_today: float
+    login_users_percentage_this_week: float
 
 
 @router.get("/", response_model=Stats, tags=["statistics"])
@@ -22,11 +24,37 @@ async def get_overall_statistics():
     limit = 10
 
     top_devices = await get_top_most_crashed_devices(limit)
-    login_users_percentage = await get_login_users_percentage_today()
+    login_users_percentage = await get_login_users_percentage_by_week()
     top_mission_reject_employees = await get_top_most_reject_mission_employee(limit)
 
     return Stats(
         top_most_crashed_devices=top_devices,
-        login_users_percentage_today=login_users_percentage,
+        login_users_percentage_this_week=login_users_percentage,
         top_most_reject_mission_employee=top_mission_reject_employees,
     )
+
+
+class WorkerStatusDto(BaseModel):
+    worker_id: str
+    worker_name: str
+    last_event_end_date: datetime.datetime
+    at_device: str
+    status: WorkerStatusEnum
+    total_dispatches: int
+
+
+@router.get("/worker-status", response_model=List[WorkerStatusDto], tags=["statistics"])
+async def get_all_worker_status():
+    states = await WorkerStatus.objects.select_related("worker").all()
+
+    return [
+        {
+            "worker_id": s.worker.username,
+            "worker_name": s.worker.full_name,
+            "at_device": s.at_device.id,
+            "status": s.status,
+            "last_event_end_date": s.last_event_end_date,
+            "total_dispatches": s.dispatch_count,
+        }
+        for s in states
+    ]
