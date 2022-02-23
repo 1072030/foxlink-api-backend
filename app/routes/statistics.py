347977cb9c696c34daccf9b3f4375c8b8a52ec93
Tony Yao <tony24862486@gmail.com>
2022-02-23 20:38:01 +0800

    feat: add more function for stats

--- app/routes/statistics.py
+++ app/routes/statistics.py
@@ -1,12 +1,32 @@
 from typing import List, Any
 from fastapi import APIRouter
+from pydantic import BaseModel
 
-from app.services.statistics import get_top_most_crashed_devices
+from app.services.statistics import (
+    get_top_most_crashed_devices,
+    get_login_users_percentage_today,
+    get_top_most_reject_mission_employee,
+)
 
 router = APIRouter(prefix="/stats")
 
 
-@router.get("/", response_model=List[Any], tags=["statistics"])
-async def get_top_crashed_devices(limit: int):
-    result = await get_top_most_crashed_devices(limit)
-    return result
+class Stats(BaseModel):
+    top_most_crashed_devices: List[Any]
+    top_most_reject_mission_employee: List[Any]
+    login_users_percentage_today: float
+
+
+@router.get("/", response_model=Stats, tags=["statistics"])
+async def get_overall_statistics():
+    limit = 10
+
+    top_devices = await get_top_most_crashed_devices(limit)
+    login_users_percentage = await get_login_users_percentage_today()
+    top_mission_reject_employees = await get_top_most_reject_mission_employee(limit)
+
+    return Stats(
+        top_most_crashed_devices=top_devices,
+        login_users_percentage_today=login_users_percentage,
+        top_most_reject_mission_employee=top_mission_reject_employees,
+    )
--- app/services/statistics.py
+++ app/services/statistics.py
@@ -1,9 +1,30 @@
-from app.core.database import database
+from app.core.database import database, User
 
 
 async def get_top_most_crashed_devices(limit: int):
-    result = await database.fetch_all(
-        f"SELECT device, count(*) AS count FROM missions GROUP BY device ORDER BY count DESC LIMIT {limit}"
+    query = await database.fetch_all(
+        f"SELECT device, count(*) AS count FROM missions GROUP BY device ORDER BY count DESC LIMIT :limit;",
+        {"limit": limit},
     )
 
-    return result
+    return query
+
+
+async def get_top_most_reject_mission_employee(limit: int):
+    query = await database.fetch_all(
+        f"SELECT user, count(*) AS count FROM `auditlogheaders` WHERE action='MISSION_REJECTED' AND MONTH(created_date) = MONTH(CURRENT_DATE()) GROUP BY user ORDER BY count DESC LIMIT :limit;",
+        {"limit": limit},
+    )
+
+    return query
+
+
+async def get_login_users_percentage_today() -> float:
+    total_user_count = await User.objects.filter(is_active=True, is_admin=False).count()
+
+    result = await database.fetch_all(
+        f"SELECT count(DISTINCT user) FROM `auditlogheaders` WHERE action='USER_LOGIN' AND created_date >= DATE(NOW()) AND created_date <= DATE(NOW()) + INTERVAL 1 DAY;"
+    )
+
+    return round(result[0][0] / total_user_count, 3)
+
