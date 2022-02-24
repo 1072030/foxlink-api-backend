991b47e68f74c24f6b0a805bdc9322fc1d029ba6
Tony Yao <tony24862486@gmail.com>
2022-02-25 00:19:52 +0800

    feat: improve log search

--- app/routes/log.py
+++ app/routes/log.py
@@ -8,19 +8,26 @@ router = APIRouter(prefix="/logs")
 
 @router.get("/", tags=["logs"])
 async def get_logs(
-    action: AuditActionEnum,
+    action: Optional[AuditActionEnum] = None,
     limit: int = 20,
+    page: int = 0,
     start_date: Optional[datetime.datetime] = None,
     end_date: Optional[datetime.datetime] = None,
 ):
     if limit <= 0:
         raise ValueError("limit must be greater than 0")
 
+    if page <= 0:
+        raise ValueError("limit must be greater than 0")
+
     params = {
-        "action": action.value,
         "created_date__gte": start_date,
         "created_date__lte": end_date,
     }
+
+    if action is not None:
+        params["action"] = action.value  # type: ignore
+
     params = {k: v for k, v in params.items() if v is not None}
 
-    return await AuditLogHeader.objects.filter(**params).all()  # type: ignore
+    return await AuditLogHeader.objects.filter(**params).paginate(page, limit).order_by("-created_date").all()  # type: ignore
