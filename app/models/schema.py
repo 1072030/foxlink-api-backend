e8e98f5cd0c132b161cee1257069383691deb1ec
Tony Yao <tony24862486@gmail.com>
2022-03-22 18:19:02 +0800

    fix: cannot use forward references

--- app/models/schema.py
+++ app/models/schema.py
@@ -50,9 +50,17 @@ class MissionUpdate(MissionBase):
     device_id: Optional[str]
 
 
+class DeviceDto(BaseModel):
+    device_id: str
+    device_name: str
+    project: str
+    process: str
+    line: int
+
+
 class MissionDto(BaseModel):
     mission_id: int
-    device: "DeviceDto"
+    device: DeviceDto
     name: str
     description: str
     assignees: List[str]
@@ -63,12 +71,4 @@ class MissionDto(BaseModel):
     event_end_date: Optional[datetime.datetime]
     created_date: datetime.datetime
     updated_date: datetime.datetime
-
-
-class DeviceDto(BaseModel):
-    device_id: str
-    device_name: str
-    project: str
-    process: str
-    line: int
 
