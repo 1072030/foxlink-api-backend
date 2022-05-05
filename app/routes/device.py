8d3bd67bc0561e4fe6b56af7bde31a3e9b6a5668
Tony Yao <tony24862486@gmail.com>
2022-05-05 21:20:56 +0800

    feat: add get_device_by_id()

--- app/routes/device.py
+++ app/routes/device.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from app.models.schema import CategoryPriorityOut, DeviceOut
-from app.services.auth import get_admin_active_user
+from app.services.auth import get_admin_active_user, get_current_active_user
 from app.core.database import CategoryPRI, Device, User
 
 router = APIRouter(prefix="/device")
@@ -23,6 +23,20 @@ async def get_all_devices(workshop_name: Optional[str] = None, user: User = Depe
     )
 
     return [DeviceOut.from_device(d) for d in devices]
+
+@router.get("/{device_id}", response_model=DeviceOut, tags=["device"])
+async def get_device_by_id(device_id: str, user: User = Depends(get_current_active_user)):
+    device = (
+        await Device.objects
+        .select_related('workshop')
+        .exclude_fields(["workshop__map", "workshop__related_devices"])
+        .filter(id=device_id).get_or_none()
+    )
+
+    if device is None:
+        raise HTTPException(404, 'the device is not found')
+
+    return DeviceOut.from_device(device)
 
 @router.get("/category-priority", response_model=List[CategoryPriorityOut], tags=["device"])
 async def get_category_priority_by_project(project: str):
