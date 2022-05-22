ea7837741245bee667e22d5957129b6534a45672
Tony Yao <tony24862486@gmail.com>
2022-05-22 19:26:55 +0800

    fix: device_id cannot get

--- app/routes/test.py
+++ app/routes/test.py
@@ -1,25 +1,25 @@
 from fastapi import APIRouter, HTTPException
 from datetime import datetime, timedelta
-from typing import List
-from app.core.database import CategoryPRI, Device, Mission, MissionEvent
-from typing import Optional
+from app.core.database import CategoryPRI, Device, FactoryMap, Mission, MissionEvent, database
 import random
 
 router = APIRouter(prefix="/test")
 
-
+@database.transaction()
 @router.post("/missions", status_code=201, tags=["test"])
-async def create_fake_mission():
-    device_count = await Device.objects.count()
-    pick_device = await Device.objects.filter(
-        id=random.randint(1, device_count)
-    ).first()
+async def create_fake_mission(workshop_name: str):
+    w = await FactoryMap.objects.exclude_fields(['image', 'map']).filter(name=workshop_name).get_or_none()
+
+    if w is None:
+        raise HTTPException(status_code=404, detail="workshop is not found")
+
+    pick_device_id = random.randint(0, len(w.related_devices) - 1)
 
     random_category = await CategoryPRI.objects.filter(
-        devices__id=pick_device.id
+        devices__id=pick_device_id
     ).first()
 
-    new_mission = await Mission.objects.create(device=pick_device, name="測試任務")
+    new_mission = await Mission.objects.create(device=pick_device_id, name="測試任務")
 
     await MissionEvent.objects.create(
         mission=new_mission,
@@ -32,7 +32,7 @@ async def create_fake_mission():
 
     return {
         "fake_mission_id": new_mission.id,
-        "device_id": pick_device.id,
+        "device_id": pick_device_id,
         "category": random_category.category,
         "message": random_category.message,
     }
