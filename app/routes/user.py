c3c835428979d58a991766ff15cc18f5dff2c16e
Tony Yao <tony24862486@gmail.com>
2022-06-02 02:17:48 +0800

    fix: device_name return none

--- app/routes/user.py
+++ app/routes/user.py
@@ -91,7 +91,7 @@ async def get_user_himself_info(user: User = Depends(get_current_active_user)):
         ).name
 
     at_device = "無"
-    worker_status = await WorkerStatus.objects.filter(worker=user).get_or_none()
+    worker_status = await WorkerStatus.objects.select_related(['at_device']).filter(worker=user).get_or_none()
     if worker_status is not None and worker_status.at_device is not None:
         at_device = worker_status.at_device.device_name
 
