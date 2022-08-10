c917098002364b70e4e72eb00c64e6691575f0a1
Tony Yao <tony24862486@gmail.com>
2022-08-10 15:38:13 +0800

    script: update update.sh again

--- update.sh
+++ update.sh
@@ -1,7 +1,14 @@
 # /bin/bash
 
-if [ "$1" = "--git"] then
+if [ "$1" = "--git" ]
+then
     git pull origin
+elif [ -z "$1" ]
+then
+    echo "Update without git pull"
+else
+    echo "Usage: update.sh --git"
+    exit 1
 fi
 
 docker-compose kill foxlink-backend && docker-compose up -d --build foxlink-backend
