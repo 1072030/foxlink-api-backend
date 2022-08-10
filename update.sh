c1a953d42ad7765aadd69d9a523a146eff6457e7
Tony Yao <tony24862486@gmail.com>
2022-08-10 15:22:02 +0800

    script: edit update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
 # /bin/bash
-git pull origin
+
+if [ "$1" = "--git"] then
+    git pull origin
+fi
+
 docker-compose kill foxlink-backend && docker-compose up -d --build foxlink-backend
 docker-compose kill foxlink-daemon && docker-compose up -d --build foxlink-daemon
