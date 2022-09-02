8d791716284a898c9fbeb67b9d8914a2157a3612
Tony Yao <tony24862486@gmail.com>
2022-09-02 13:58:47 +0800

    chore: update cors record

--- app/main.py
+++ app/main.py
@@ -33,6 +33,7 @@ origins = [
     "http://localhost:8080",
     "http://140.118.157.9:43114",
     "http://192.168.65.210:8083",
+    "http://140.118.157.9:8086",
     "http://ntust.foxlink.com.tw",
 ]
 
